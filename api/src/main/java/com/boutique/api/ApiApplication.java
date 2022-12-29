a3759c47750272fec478c474b9f07a283078affb
mohamed elhamra <melhamra98@gmail.com>
2022-12-29 02:38:18 +0100

    feat (api) : ajout de la documentation
    Ajouter la documentation au niveau du controlleur

--- api/src/main/java/com/boutique/api/ApiApplication.java
+++ api/src/main/java/com/boutique/api/ApiApplication.java
@@ -1,9 +1,17 @@
 package com.boutique.api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "API pour la gestion des boutiques",
+				version = "1.0",
+				description = "Cette application a pour but la gestion des boutiqes, leurs produits et ses catégories"
+		))
 public class ApiApplication {
 
 	public static void main(String[] args) {
--- api/src/main/java/com/boutique/api/controllers/CategorieController.java
+++ api/src/main/java/com/boutique/api/controllers/CategorieController.java
@@ -2,6 +2,11 @@ package com.boutique.api.controllers;
 
 import com.boutique.api.dtos.CategorieDto;
 import com.boutique.api.services.CategorieService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,11 +16,17 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/categories")
 @AllArgsConstructor
+@Tag(name = "Controlleur de catégorie", description = "La gestion des catégories")
 public class CategorieController {
 
     private CategorieService categorieService;
 
     @PostMapping
+    @ApiOperation(value = "Créer une catégorie", response = CategorieDto.class, tags = "createCategorie")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Opération réussite"),
+            @ApiResponse(responseCode= "400", description = "BadRequest") }
+    )
     public ResponseEntity<CategorieDto> createCategorie(@RequestBody @Valid CategorieDto categorieDto){
         return ResponseEntity.ok(categorieService.createCategorie(categorieDto));
     }
