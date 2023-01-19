b60b0361a2b700ff33796cc6e5e33c3b98ebc35d
mohamed elhamra <melhamra98@gmail.com>
2023-01-19 21:12:45 +0100

    feat (api) : Change DB schema
    Change entities, store can open many times in day

--- api/src/main/java/com/boutique/api/ApiApplication.java
+++ api/src/main/java/com/boutique/api/ApiApplication.java
@@ -7,15 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @OpenAPIDefinition(
-		info = @Info(
-				title = "API pour la gestion des boutiques",
-				version = "1.0",
-				description = "Cette application a pour but la gestion des boutiqes, leurs produits et ses catégories"
-		))
+        info = @Info(
+                title = "API pour la gestion des boutiques",
+                version = "1.0",
+                description = "Cette application a pour but la gestion des boutiqes, leurs produits et ses catégories"
+        ))
 public class ApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
 
 }
