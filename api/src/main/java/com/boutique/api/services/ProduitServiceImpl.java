5619f13e030e0148a418b37bc0ec601c7db70b92
mohamed elhamra <melhamra98@gmail.com>
2023-02-16 16:56:01 +0100

    feat (api) : init db
    initialisation de la DB lors du premier démarrage

--- api/src/main/java/com/boutique/api/services/ProduitServiceImpl.java
+++ api/src/main/java/com/boutique/api/services/ProduitServiceImpl.java
@@ -15,6 +15,7 @@ import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.Set;
