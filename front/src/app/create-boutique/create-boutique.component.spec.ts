a16e38a39610863b7a15e5e67c27dcf137088536
MohammedMaimouni <maimounim98@gmail.com>
2023-02-16 01:28:47 +0100

    feat (front) : inti component boutique et classe boutique Création de la classe et le component(sans fonctionnalités)

---
+++ front/src/app/create-boutique/create-boutique.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CreateBoutiqueComponent } from './create-boutique.component';
+
+describe('CreateBoutiqueComponent', () => {
+  let component: CreateBoutiqueComponent;
+  let fixture: ComponentFixture<CreateBoutiqueComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CreateBoutiqueComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CreateBoutiqueComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
